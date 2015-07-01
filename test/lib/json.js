3b392c9524cea2825f4d26deec8628d2a26e5187
Ralph Khattar <ralph.khattar@gmail.com>
2015-07-01 18:15:16 -0400

    Moved JSON try parse logic into helper.

---
+++ test/lib/json.js
@@ -0,0 +1,13 @@
+var json = require('../../lib/json');
+var assert = require('assert');
+
+describe('json', function () {
+
+	describe('tryParse', function () {
+
+		it('returns null for invalid JSON ', function () {
+			assert(null === json.tryParse('INVALID JSON'));
+		});
+
+	});
+});
